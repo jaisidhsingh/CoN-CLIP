2df6be02ab030ff0c64ee8f4415b745c7f49530e
Ishaan Shrivastava <ishaanshri95@gmail.com>
2025-05-10 05:32:47 +0530

    Normalize line endings

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -5,12 +5,11 @@ configs = SimpleNamespace(**{})
 
 
 ## CC-Neg (for fine-tuning and evaluation)
-configs.ccneg_root_folder = "../../ccneg_dataset"         ## change this accordingly
-configs.finetuning_dataset_path = f"{configs.ccneg_root_folder}/ccneg_preprocessed.pt"
+configs.ccneg_root_folder = "C://personal/conclip_wrap/CoN-CLIP/ccneg_dataset"         ## change this accordingly                                    
+configs.finetuning_dataset_path = f"{configs.ccneg_root_folder}/ccneg_preprocessed.pt"                                  
 configs.negative_image_ft_mapping_path = f"{configs.ccneg_root_folder}/distractor_image_mapping.pt"
 configs.num_ccneg_eval_samples = 40000  ## the last 40,000 indices consist of the decided evaluation split for CC-Neg
-
-## MS-COCO (for finetuning)
+## MS-COCO (for finetuning)                                                                                             
 configs.coco_root_folder = "/workspace/datasets/coco"           ## change this accordingly
-configs.negative_image_dataset_root = f"{configs.coco_root_folder}/coco_train2017/train2017"    ## path to coco train2017 images
+configs.negative_image_dataset_root = f"{configs.coco_root_folder}/coco_train2017/train2017"    ## path to coco train2017 images                                                                                                                
 configs.negative_image_dataset_annotations_path = f"{configs.coco_root_folder}/coco_ann2017/annotations/captions_train2017.json"    ## path to coco train2017 annotations (not used but required by torchvision dataset)
